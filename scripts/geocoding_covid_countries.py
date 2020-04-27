73014e8a983481b96367609924434bf3f6583176
aladoy <anais.ladoy@epfl.ch>
2020-04-27 19:02:08 +0200

    geocoding of Covid cases + update geocoding of Google Trends

---
+++ scripts/geocoding_covid_countries.py
@@ -0,0 +1,25 @@
+#geocoding_covid_countries.py
+
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+
+#Import Covid-19 cases data
+data = pd.read_csv('docs/data/covid_cases_2020-04-01.csv',sep=';')
+
+#Import geojson file with european countries
+countries=gpd.read_file('docs/data/europe_countries.geojson')
+
+#Convert data to GeoDataframe
+data=gpd.GeoDataFrame(data, geometry=gpd.points_from_xy(data.Long, data.Lat), crs={'init': 'epsg:4326'})
+
+#Spatial join with europe_countries
+geocoded=gpd.sjoin(countries[['id','geometry']],data,how='inner',op='intersects')
+
+#Final wrangling
+geocoded['Date'] = pd.to_datetime(geocoded['Date'])
+geocoded=geocoded[['Date','id','Lat','Long','Confirmed','Deaths']]
+geocoded.columns=['date','region_id','lat','lon','covid_confirmed','covid_deaths']
+
+#Save to CSV
+geocoded.to_csv('docs/data/geocoded_covid_cases.csv',index=False)
--- scripts/geocoding_trends_countries.py
+++ scripts/geocoding_trends_countries.py
@@ -7,12 +7,17 @@ import numpy as np
 #Import Google Trends data
 data = pd.read_csv('docs/data/interest_by_country.csv')
 
+#Fix country codes
+data.loc[data.geoName=='Greece','geoCode']='EL' #GR->EL
+data.loc[data.geoName=='United Kingdom','geoCode']='UK' #GB->UK
+
 #Import geojson file with european countries
 countries=gpd.read_file('docs/data/europe_countries.geojson')
 
 #Keep only countries from geojson
 data=data[data.geoCode.isin(countries.id)]
 
+data.drop('geoName',axis=1,inplace=True)
 #Rename columns
 data.rename(columns={'Unnamed: 0':'date','geoCode':'country_id','Coronavirus':'trends_covid'},inplace=True)
 
