8c5fbe3b155b4a395be72b42bcb359a87bcbf594
aladoy <anais.ladoy@epfl.ch>
2020-05-26 16:41:08 +0200

    add new covariates

--- scripts/reshape_demographics.R
+++ scripts/reshape_demographics.R
@@ -10,12 +10,12 @@ pop$age<-as.factor(pop$age)
 pop$sex<-as.factor(pop$sex)
 pop$pop<-as.numeric(pop$pop)
 
-#Add categorie Y10_35 and reshape dataframe
-pop <- pop %>% spread(key='age', value='pop') %>% mutate(Y10_35=`Y10-14`+`Y15-19`+`Y20-24`+`Y25-29`+`Y30-34`) %>% select(c('geo','sex','TOTAL','Y10_35'))
-pop <- pop %>% pivot_wider(id_cols='geo', names_from='sex',values_from =c('TOTAL','Y10_35'))
+#Add categorie young and reshape dataframe
+pop <- pop %>% spread(key='age', value='pop') %>% mutate(young=`Y_LT5`+`Y5-9`+`Y10-14`, elderly=`Y65-69`+`Y70-74`+`Y_GE75`) %>% select(c('geo','sex','TOTAL','young','elderly'))
+pop <- pop %>% pivot_wider(id_cols='geo', names_from='sex',values_from =c('TOTAL','young','elderly'))
 
-#Convert female / male / Y10_35 to proportion [%]
-pop <- pop %>% mutate(pop=TOTAL_total, female=(TOTAL_female*100)/TOTAL_total, male=(TOTAL_male*100)/TOTAL_total, Y10_35=(Y10_35_total*100)/TOTAL_total) %>% select(c('geo','pop','female','male','Y10_35'))
+#Convert female / male / young to proportion [%]
+pop <- pop %>% mutate(pop=TOTAL_total, female=(TOTAL_female*100)/TOTAL_total, male=(TOTAL_male*100)/TOTAL_total, young=(young_total*100)/TOTAL_total, elderly=(elderly_total*100)/TOTAL_total) %>% select(c('geo','pop','female','male','young','elderly'))
 
 #Save results
 write_csv(pop,'../docs/data/demography_by_country.csv')
